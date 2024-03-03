f3c5e6fe86968e56e29c63c423fc11d83f6660ab
윤범태 <yoonbumtae@gmail.com>
2024-03-04 00:43:50 +0900

    Fix: ignore to push Pods file

--- Pods/Target Support Files/Pods-MusicScale-MusicScaleUITests/Pods-MusicScale-MusicScaleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicScale_MusicScaleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MusicScale_MusicScaleUITests
-@end
--- Pods/Target Support Files/Pods-MusicScale/Pods-MusicScale-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicScale : NSObject
-@end
-@implementation PodsDummy_Pods_MusicScale
-@end
